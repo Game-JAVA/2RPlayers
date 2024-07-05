657cacef723c72ab7246d4bee65ca16db70520d2
H4ckta <joao.dev007@gmail.com>
2024-07-05 14:18:44 -0300

    code: deliverypigeon/DeliveryPigeon.java
    Adding the restart method when you die

--- deliverypigeon/DeliveryPigeon.java
+++ deliverypigeon/DeliveryPigeon.java
@@ -106,4 +106,14 @@ public class DeliveryPigeon extends JPanel implements ActionListener, KeyListene
         parentFrame.revalidate();
         parentFrame.repaint();
     }
+
+    private void restartGame() {
+        pigeon.setY(pigeonY);
+        velocityY = 0;
+        pipes.clear();
+        gameOver = false;
+        score = 0;
+        gameLoop.start();
+        placePipeTimer.start();
+    }
 }
