400b76a7c6e61e4413eb64b5eae30f8ff221adc4
luizgustavo200555 <158209768+luizgustavo200555@users.noreply.github.com>
2024-07-11 16:18:19 -0300

    code: deliverypigeon/Main.java
    adding screen creation functionality

--- deliverypigeon/Main.java
+++ deliverypigeon/Main.java
@@ -1,5 +1,10 @@
 public class Main{
     public static void main(String[] args){
-        
+           SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Delivery Pigeon Game");
+            InicioTela inicioTela = new InicioTela(frame);
+            frame.add(inicioTela);
+            frame.pack();
+        });
     }
 }
