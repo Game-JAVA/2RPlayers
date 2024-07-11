91c19d548bd0adaba94e00d9f9248c0a152a700e
sinkevirns <rafaelsinkevicius07@gmail.com>
2024-07-11 16:39:48 -0300

    code: deliverypigeon/App.java
    Add board width

--- deliverypigeon/App.java
+++ deliverypigeon/App.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            
+            int boardWidth = 500;
         });
     }
 }
