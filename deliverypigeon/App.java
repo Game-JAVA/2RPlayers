ff873171e433db42df547682d0a194e669b3077e
sinkevirns <rafaelsinkevicius07@gmail.com>
2024-07-11 16:43:13 -0300

    code: deliverypigeon/App.java
    Add set location relative to

--- deliverypigeon/App.java
+++ deliverypigeon/App.java
@@ -8,6 +8,7 @@ public class App {
 
             JFrame frame = new JFrame("Delivery Pigeon");
             frame.setSize(boardWidth, boardHeight);
+            frame.setLocationRelativeTo(null);
         });
     }
 }
