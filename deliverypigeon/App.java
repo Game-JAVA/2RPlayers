1618a6634604677fabcecc07bc6c05e06fe41ea5
sinkevirns <rafaelsinkevicius07@gmail.com>
2024-07-11 16:40:19 -0300

    code: deliverypigeon/App.java
    Add board height

--- deliverypigeon/App.java
+++ deliverypigeon/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             int boardWidth = 500;
+            int boardHeight = 650;
         });
     }
 }
