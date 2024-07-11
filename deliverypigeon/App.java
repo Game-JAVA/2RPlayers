87fe8142b3512df8ed72046dddb00ed4819115a3
sinkevirns <rafaelsinkevicius07@gmail.com>
2024-07-11 16:45:36 -0300

    code: deliverypigeon/App.java
    Add InicioTela

--- deliverypigeon/App.java
+++ deliverypigeon/App.java
@@ -11,6 +11,8 @@ public class App {
             frame.setLocationRelativeTo(null);
             frame.setResizable(false);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            InicioTela inicioTela = new InicioTela(frame);
         });
     }
 }
