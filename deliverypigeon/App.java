a9f8a7beb2cbdc6616b6bd050f2b2117fb7fce11
sinkevirns <rafaelsinkevicius07@gmail.com>
2024-07-11 16:46:32 -0300

    code: deliverypigeon/App.java
    Add frame

--- deliverypigeon/App.java
+++ deliverypigeon/App.java
@@ -13,6 +13,7 @@ public class App {
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
             InicioTela inicioTela = new InicioTela(frame);
+            frame.add(inicioTela);
         });
     }
 }
